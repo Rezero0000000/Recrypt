a7ccb0e014665adf57a9f2750cb2423dc4ae4647
Rekun <hikarisakata91@gmail.com>
2023-05-21 16:22:28 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,13 @@
-print("=======--- [ Welcome ] ---=======")
+import os as os
 
+os.system("cls")
+print("=======--- [ Welcome ] ---=======\n")
+print("[1]. Encrypt\n[2].Decrypt\n")
+status = int(input("Chose option: "))
+
+if (status == 1):
+    print('1')
+elif (status == 2):
+    print('2')
+else:
+    print("Wrong Option")
