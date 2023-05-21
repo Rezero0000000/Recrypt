441e46d2ba591eb9c1eb328f928eb6ac7b5a1735
Rekun <hikarisakata91@gmail.com>
2023-05-21 16:27:23 +0700

    Update index.py

--- index.py
+++ index.py
@@ -2,11 +2,22 @@ import os as os
 
 os.system("cls")
 print("=======--- [ Welcome ] ---=======\n")
-print("[1]. Encrypt\n[2].Decrypt\n")
+print("[1]. Encrypt\n[2]. Decrypt\n")
 status = int(input("Chose option: "))
 
+def method ():
+    os.system("cls")
+
+    print("=======--- [ Chose cipher ] ---=======\n")
+    print("[1]. Affine\n[2]. Base64\n[3]. Caesar\n[4]. Hill\n[5]. RandomAlfa\n[6]. Rot\n[7]. Transposition\[8]. Vigenere\n")
+    status = int(input("Chose option: "))
+
+    return status
+
+
 if (status == 1):
-    print('1')
+    cipher = method()
+    print(cipher)
 elif (status == 2):
     print('2')
 else:
