5d15b4a51be77d09fe37799863075c92980d148b
Rekun <hikarisakata91@gmail.com>
2023-10-03 19:28:09 +0700

    update cipher

--- rot.py
+++ rot.py
@@ -1,46 +1,56 @@
 rot13 = 'abcdefghijklmnopqrstuvwxyz'
 rot18 = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-def rotEnc (plainText, key):
+def rotEnc(plainText, key):
     global rot13
     global rot18
 
     cipherText = ""
 
-    for i in range(0, len(plainText)):
-        if plainText[i] == ' ':
-            cipherText = cipherText + " "
+    for char in plainText:
+        if char == ' ':
+            cipherText += " "
+
         elif key == "rot13":
-            for j in range(0, len(rot13)):
-
-                if plainText[i] == rot13[j]:
-                    if j < 13 and j >= 0:
-                        cipherText = cipherText + rot13[j + 13]
-                    elif j > 13 and j <= 25:
-                        cipherText = cipherText + rot13[j - 13]
-                    elif j == 13:
-                        cipherText = cipherText + "a"
-        elif key == "rot18":
-            for j in range (0, len(rot18)):
-
-                if plainText[i] == rot18[j]:
-                    if j < 18 and j >= 0:
-                        cipherText = cipherText + rot18[j + 18]
-                    elif j > 18 and j <= 35:
-                        cipherText = cipherText + rot18[j - 18]
+            if char.isalpha():
+                is_upper = char.isupper()
+                char = char.lower()
+                if char in rot13:
+                    index = rot13.index(char)
+                    if is_upper:
+                        cipherText += rot13[index - 13].upper()
                     else:
-                        cipherText = cipherText + "a"
+                        cipherText += rot13[index - 13]
+                else:
+                    cipherText += char
+            else:
+                cipherText += char
+
+        elif key == "rot18":
+            if char.isalnum():
+                if char.isdigit():
+                    index = rot18.index(char)
+                    cipherText += rot18[(index + 18) % 36]
+                else:
+                    is_upper = char.isupper()
+                    char = char.lower()
+                    if char in rot18:
+                        index = rot18.index(char)
+                        if is_upper:
+                            cipherText += rot18[index - 18].upper()
+                        else:
+                            cipherText += rot18[index - 18]
+            else:
+                cipherText += char
         else:
-            print("woi salah kampret")
-                        
+            print("Kunci yang salah")
+
     print(cipherText, '\n')
 
+# Exmaple 
+rotEnc("oyasumi oyasumi close your eyes and you leave this dream", 'rot13')
+rotEnc("blnfhzv blnfhzv pybfr lbhe rlrf naq lbh yrnir guvf qernz", 'rot13')
 
-# Example
-
-#rotEnc("oyasumi oyasumi close your eyes and you leave this dream", 'rot13')
-#rotEnc("blnfhzv blnfhzv pybfr lbhe rlrf naq lbh yrnir guvf qernz","rot13")
-
-#rotEnc("oyasumi oyasumi close your eyes and you leave this dream", 'rot18')
-#rotEnc("6gsac40 6gsac40 u36aw g6c9 wgwa s5v g6c 3wsdw bz0a v9ws4", 'rot18')
+rotEnc("oyasumi oyasumi close your eyes and you leave this dream", 'rot18')
+rotEnc("6gsac40 6gsac40 u36aw g6c9 wgwa s5v g6c 3wsdw bz0a v9ws4", 'rot18')
 
--- transposition.py
+++ transposition.py
@@ -1,32 +1,42 @@
-def enctrans (plainText, key):
-    while len(plainText)%key > 0:
+def enctrans(plainText, key):
+    while len(plainText) % key > 0:
         plainText = plainText + "#"
 
-    n = len(plainText)//key
+    n = len(plainText) // key
     cipherText = ""
 
     for i in range(key):
         for j in range(n):
-            cipherText = cipherText + plainText[i+j*key]
-            
+            cipherText = cipherText + plainText[i + j * key]
+
     return cipherText
 
-def dectrans (cipherText, key):
-    while len(cipherText)%key > 0:
-        cipherText = cipherText + "#" 
+def dectrans(cipherText, key):
+    while len(cipherText) % key > 0:
+        cipherText = cipherText + "#"
 
-    key = len(cipherText)//key
-    n = len(cipherText)//key
+    key = len(cipherText) // key
+    n = len(cipherText) // key
 
     plainText = ""
 
     for i in range(key):
         for j in range(n):
-            if cipherText[i+j*key] == "#":
-                cipherText = cipherText + ""
+            if cipherText[i + j * key] == "#":
+                pass
             else:
-                plainText = plainText + cipherText[i+j*key]
+                plainText = plainText + cipherText[i + j * key]
 
     return plainText
 
+# Example
+
+plainText = "Hello, World! 123"
+key = 5
+encryptedText = enctrans(plainText, key)
+decryptedText = dectrans(encryptedText, key)
+
+print("Pesan Asli:", plainText)
+print("Pesan Terenkripsi:", encryptedText)
+print("Pesan Terdekripsi:", decryptedText)
 
