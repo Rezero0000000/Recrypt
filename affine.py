08b9355f5d64c838d1b183226687918ebfe328e5
Rekun <hikarisakata91@gmail.com>
2023-10-03 19:24:19 +0700

    meodif everything to use ord

--- affine.py
+++ affine.py
@@ -1,45 +1,63 @@
-alfabet = "abcdefghijklmnopqrstuvwxyz"
-
-def invers ():
+def invers():
     a_inv = 0
 
     for i in range(26):
-        flag = (3 * i)%26
+        flag = (3 * i) % 26
         if flag == 1:
             a_inv += i
 
     return a_inv
 
-def affEnc (plainText, keyA, keyB):
-    global alfabet
+def affEnc(plainText, keyA, keyB):
     cipherText = ""
-    
-    for i in range(0, len(plainText)):
 
-        if plainText[i] == " ":
-            cipherText = cipherText + " "
+    for char in plainText:
 
-        for j in range(0, len(alfabet)):
+        if char == " ":
+            cipherText += " "
 
-            if plainText[i] == alfabet[j]:
-                alfaIndex = ((j*keyA) + keyB) % 26
-                cipherText = cipherText + alfabet[alfaIndex]
+        elif char.isalpha():
+
+            is_upper = char.isupper()
+            char = char.lower()
+            ascii_val = ord(char) - ord('a')
+            alfaIndex = ((ascii_val * keyA) + keyB) % 26
+            cipherChar = chr(alfaIndex + ord('a'))
+
+            if is_upper:
+                cipherChar = cipherChar.upper()
+            cipherText += cipherChar
 
     return cipherText
 
-def affDec (cipherText, keyA, keyB):
-    global alfabet
+def affDec(cipherText, keyA, keyB):
     plainText = ""
 
-    for i in range(0, len(cipherText)):
+    for char in cipherText:
 
-        if cipherText[i] == " ":
-            plainText = plainText + " "
+        if char == " ":
+            plainText += " "
 
-        for j in range(0, len(alfabet)):
-          
-            if cipherText[i] == alfabet[j]:
-                alfaIndex = ((invers() * (j  - keyB)) % 26)
-                plainText = plainText + alfabet[alfaIndex]
+        elif char.isalpha():
+            is_upper = char.isupper()
+            char = char.lower()
+            ascii_val = ord(char) - ord('a')
+            alfaIndex = ((invers() * (ascii_val - keyB)) % 26)
+            plainChar = chr(alfaIndex + ord('a'))
+
+            if is_upper:
+                plainChar = plainChar.upper()
+            plainText += plainChar
 
     return plainText
+
+plainText = "Hello, World! 123"
+keyA = 5
+keyB = 7
+
+encrypted = affEnc(plainText, keyA, keyB)
+decrypted = affDec(encrypted, keyA, keyB)
+
+print("Pesan Asli:", plainText)
+print("Pesan Terenkripsi:", encrypted)
+print("Pesan Terdekripsi:", decrypted)
