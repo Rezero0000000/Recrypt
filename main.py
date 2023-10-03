defa41500798500c15d6cb424414cd74220e66f7
Rekun <hikarisakata91@gmail.com>
2023-10-03 19:57:48 +0700

    Update UI

---
+++ main.py
@@ -0,0 +1,19 @@
+icon = """
+
+
+  ____                            _   
+ |  _ \ ___  ___ _ __ _   _ _ __ | |_ 
+ | |_) / _ \/ __| '__| | | | '_ \| __|
+ |  _ <  __/ (__| |  | |_| | |_) | |_ 
+ |_| \_\___|\___|_|   \__, | .__/ \__|
+                      |___/|_|      
+
+ ------- [ Created by Schwatz] -------
+ 
+ [1]. Encryption\n [2]. Decryption
+"""
+
+print(icon)
+option = input(" Your option : ")
+
+print(option)
